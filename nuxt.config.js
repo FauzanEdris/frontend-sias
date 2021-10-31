149caff1313ab395d759f8bb2a5894ff03597765
greydios vandita <greydios.vandita@gmail.com>
2021-10-31 23:58:59 +0800

    init: first commit greydios

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,23 +1,34 @@
 export default {
+  server: {
+    port: 8080, // default: 3000
+    host: '0.0.0.0' // default: localhost,
+  },
+  mode: 'universal',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'frontend-sias',
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      // { rel: 'png', type: 'image/x-png', href: '/icon_stikom.png' }
     ]
   },
-
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#87CEFA' },
+  // #6495ED
+  // #00BFFF
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
   ],
-
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    // { src: '~/plugins/socket.client.js' }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -26,23 +37,55 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/eslint
+    // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    // Doc: https://bootstrap-vue.js.org
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
+    // Doc: https://github.com/js-cookie/js-cookie/tree/latest#readme
+    'js-cookie',
+    'cookieparser',
+    'nuxt-socket-io',
     // https://go.nuxtjs.dev/axios
+    // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa'
   ],
-
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  axios: {
+    proxy: true
+  },
+  proxy: {
+    // '/api': 'http://192.168.217.1:3000'
+    // '/api': 'http://192.168.43.131:3000/'
+    '/api': 'http://localhost:3000'
+    // '/api': 'http://10.164.236.241:3000'
 
+  },
+  io: {
+    // module options
+    sockets: [{
+      name: 'main',
+      // url: 'http://192.168.43.131:3000'
+      url: 'http://localhost:3000'
+      // url: 'http://10.164.236.241:3000'
+    }]
+  },
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,111 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        frontend-sias
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <b-form id="formLogin" ref="form" @submit.prevent="login">
+      <b-form-group invalid-feedback="Harap Isi Username" label-for="form-id_user">
+        <b-form-input
+          ref="id_user"
+          v-model="form.username"
+          type="text"
+          placeholder="Masukkan Username"
+          required
+        />
+      </b-form-group>
+      <b-form-group invalid-feedback="harap Isi Passowrd" label-for="form-password">
+        <b-form-input
+          ref="password"
+          v-model="form.password"
+          type="password"
+          placeholder="Masukkan Password"
+          required
+        />
+      </b-form-group>
+      <b-form-group>
+        <b-button type="submit">
+          Login
+        </b-button>
+      </b-form-group>
+    </b-form>
   </div>
 </template>
 
 <script>
-export default {}
+const cookie = process.client ? require('js-cookie') : undefined
+
+export default {
+  middleware: 'notAuthenticated',
+  data () {
+    return {
+      form: {
+        username: null,
+        password: null
+      },
+      msg: null
+    }
+  },
+  methods: {
+    async login () {
+      const exp = new Date(new Date().getTime() + 60 * 60 * 1000)
+      const res = await this.$store.dispatch('LOGIN', this.form)
+      if (res.status === 'success') {
+        // const role = res.data.role
+        const token = await res.data.token.split('.')
+        // cookie.set('x-access-token', 'Ayam ' + res.data.token + ' Bebek', { expires: exp })
+        await cookie.set('token', 'Ayam ' + token[0] + ' Bebek', { expires: exp })
+        await cookie.set('x', 'Ayam ' + token[2] + ' Bebek', { expires: exp })
+        await cookie.set('access', 'Ayam ' + token[1] + ' Bebek', { expires: exp })
+        await this.$store.dispatch('ME', token[2] + '.' + token[1] + '.' + token[0])
+        // switch (role) {
+        //   case 'Admin':
+        //     this.msg = res.msg
+        //     this.$router.push('/admin')
+        //     break
+        //   case 'Operator':
+        //     return this.$router.push('/operator')
+        //   case 'Dosen':
+        //     return this.$router.push('/dosen')
+        //   case 'Akademik':
+        //     this.$router.push('/akademik')
+        //     break
+        //   case 'Keuangan':
+        //     this.$router.push('/keuangan')
+        //     break
+        //   case 'Asdos':
+        //     this.$router.replace('/asdos')
+        //     break
+        //   default:
+        //     this.$router.push('/')
+        //     break
+        // }
+      } else {
+        this.msg = res.msg
+      }
+    }
+    // redirect ({ res, role }) {
+    //   switch (role) {
+    //     case 'Admin':
+    //       this.msg = res.msg
+    //       this.$router.push('/admin')
+    //       break
+    //     case 'Operator':
+    //       return this.$router.push('/operator')
+    //     case 'Dosen':
+    //       return this.$router.push('/dosen')
+    //     case 'Akademik':
+    //       this.$router.push('/akademik')
+    //       break
+    //     case 'Keuangan':
+    //       this.$router.push('/keuangan')
+    //       break
+    //     case 'Asdos':
+    //       this.$router.replace('/asdos')
+    //       break
+    //     default:
+    //       this.$router.push('/')
+    //       break
+    //   }
+    // }
+  }
+}
 </script>
 
 <style>
@@ -42,16 +119,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
