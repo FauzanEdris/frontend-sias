afe5a8a1ee9937b1eb865f191ffdf5f0eba8ec16
greydios vandita <greydios.vandita@gmail.com>
2021-12-03 13:56:56 +0800

    tampilan change password

--- components/navbar.vue
+++ components/navbar.vue
@@ -40,10 +40,60 @@
           </b-button>
         </b-nav-form> -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item @click="logout"> Logout </b-nav-item>
+          <b-nav-item-dropdown text="Profil" right>
+            <b-dropdown-item href="#">
+              Account
+            </b-dropdown-item>
+            <b-dropdown-item v-b-modal.modal-prevent-closing>
+              Change password
+            </b-dropdown-item>
+            <b-dropdown-item @click="logout">
+              Logout
+            </b-dropdown-item>
+            </b-dropdown-item>
+          </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
+    <b-modal
+      id="modal-prevent-closing"
+      ref="modal"
+      title="Change Password"
+      @show="resetModal"
+      @hidden="resetModal"
+      @ok="handleOk"
+    >
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <b-form-group
+          label="Enter your Old Password"
+          label-for="password-input"
+          invalid-feedback="Password is required"
+          :state="passwordState"
+        >
+          <b-form-input
+            id="name-input"
+            v-model="password"
+            type="password"
+            :state="passwordState"
+            required
+          />
+          <b-form-group
+            label="Enter your New Password"
+            label-for="password-input"
+            invalid-feedback="Password is required"
+            :state="nameState"
+          >
+            <b-form-input
+              id="password-input"
+              v-model="password"
+              type="password"
+              :state="passwordState"
+              required
+            />
+          </b-form-group>
+        </b-form-group>
+      </form>
+    </b-modal>
   </div>
 </template>
 
