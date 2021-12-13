a6b414eded1e0c8293daac5323b524a5b58775a5
greydios vandita <greydios.vandita@gmail.com>
2021-12-13 18:37:44 +0800

    tampilan profil dinamis (1)

--- pages/admin/profil.vue
+++ pages/admin/profil.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container">
     <div class="main-body">
-      <div class="row">
-        <div class="col-lg-4">
+      <div class="row gutters-sm">
+        <div class="col-md-4 mb-3">
           <div class="card">
             <div class="card-body">
               <div class="d-flex flex-column align-items-center text-center">
-                <img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
+                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
                 <div class="mt-3">
-                  <h4>John Doe</h4>
+                  <h3>John</h3>
                   <p class="text-secondary mb-1">
                     Full Stack Developer
                   </p>
@@ -21,110 +21,112 @@
                   <button class="btn btn-outline-primary">
                     Message
                   </button>
+                  </a>
                 </div>
               </div>
-              <hr class="my-4">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                  <h6 class="mb-0">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="feather feather-globe me-2 icon-inline"
-                    ><circle cx="12" cy="12" r="10" /><line x1="2" y1="12" x2="22" y2="12" /><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" /></svg>Website
-                  </h6>
-                  <span class="text-secondary">https://bootdey.com</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                  <h6 class="mb-0">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="feather feather-github me-2 icon-inline"
-                    ><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>Github
-                  </h6>
-                  <span class="text-secondary">bootdey</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                  <h6 class="mb-0">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="feather feather-twitter me-2 icon-inline text-info"
-                    ><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" /></svg>Twitter
-                  </h6>
-                  <span class="text-secondary">@bootdey</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                  <h6 class="mb-0">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="feather feather-instagram me-2 icon-inline text-danger"
-                    ><rect
-                      x="2"
-                      y="2"
-                      width="20"
-                      height="20"
-                      rx="5"
-                      ry="5"
-                    /><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" /><line x1="17.5" y1="6.5" x2="17.51" y2="6.5" /></svg>Instagram
-                  </h6>
-                  <span class="text-secondary">bootdey</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                  <h6 class="mb-0">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="feather feather-facebook me-2 icon-inline text-primary"
-                    ><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>Facebook
-                  </h6>
-                  <span class="text-secondary">bootdey</span>
-                </li>
-              </ul>
             </div>
           </div>
+          <div class="card mt-3">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                <h6 class="mb-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-globe mr-2 icon-inline"
+                  ><circle cx="12" cy="12" r="10" /><line x1="2" y1="12" x2="22" y2="12" /><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" /></svg>Website
+                </h6>
+                <span class="text-secondary">https://bootdey.com</span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                <h6 class="mb-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-github mr-2 icon-inline"
+                  ><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>Github
+                </h6>
+                <span class="text-secondary">bootdey</span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                <h6 class="mb-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-twitter mr-2 icon-inline text-info"
+                  ><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" /></svg>Twitter
+                </h6>
+                <span class="text-secondary">@bootdey</span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                <h6 class="mb-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-instagram mr-2 icon-inline text-danger"
+                  ><rect
+                    x="2"
+                    y="2"
+                    width="20"
+                    height="20"
+                    rx="5"
+                    ry="5"
+                  /><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" /><line x1="17.5" y1="6.5" x2="17.51" y2="6.5" /></svg>Instagram
+                </h6>
+                <span class="text-secondary">bootdey</span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                <h6 class="mb-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-facebook mr-2 icon-inline text-primary"
+                  ><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>Facebook
+                </h6>
+                <span class="text-secondary">bootdey</span>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="col-lg-8">
-          <div class="card">
+        <div class="col-md-8">
+          <div class="card mb-3">
             <div class="card-body">
-              <div class="row mb-3">
+              <div class="row">
                 <div class="col-sm-3">
                   <h6 class="mb-0">
                     Full Name
@@ -138,13 +140,14 @@
                     :disabled="!isEditing"
                     :class="{view: !isEditing}"
                     :value="user.name"
-                  >
+                  ></a>
                 </div>
               </div>
-              <div class="row mb-3">
+              <hr>
+              <div class="row">
                 <div class="col-sm-3">
                   <h6 class="mb-0">
-                    email
+                    Email
                   </h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
@@ -158,7 +161,8 @@
                   >
                 </div>
               </div>
-              <div class="row mb-3">
+              <hr>
+              <div class="row">
                 <div class="col-sm-3">
                   <h6 class="mb-0">
                     Phone
@@ -175,10 +179,11 @@
                   >
                 </div>
               </div>
-              <div class="row mb-3">
+              <hr>
+              <div class="row">
                 <div class="col-sm-3">
                   <h6 class="mb-0">
-                    Nomor HP
+                    Mobile
                   </h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
@@ -192,10 +197,11 @@
                   >
                 </div>
               </div>
-              <div class="row mb-3">
+              <hr>
+              <div class="row">
                 <div class="col-sm-3">
                   <h6 class="mb-0">
-                    Alamat
+                    Address
                   </h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
@@ -209,9 +215,9 @@
                   >
                 </div>
               </div>
+              <hr>
               <div class="row">
-                <div class="col-sm-3" />
-                <div class="col-sm-9 text-secondary">
+                <div class="col-sm-12">
                   <input
                     v-if="!isEditing"
                     type="button"
@@ -244,9 +250,7 @@
 </template>
 
 <script>
-
 export default {
-
   data () {
     return {
       isEditing: false,
@@ -255,7 +259,7 @@ export default {
         email: 'dios.comsa',
         phone: '0361241124',
         mobile: '08123123124',
-        address: 'jalan raya i gusti ngurah rai no 1 badung'
+        address: 'jalan raya i gusti ngurah rai no 1'
 
       }
     }
@@ -273,13 +277,21 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="css" scoped>
 body{
-    background: #f7f7ff;
     margin-top:20px;
+    color: #1a202c;
+    text-align: left;
+    background-color: #e2e8f0;
+}
+.main-body {
+    padding: 15px;
 }
 .card {
-  margin-top: 1rem;
+    box-shadow: 0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06);
+}
+
+.card {
     position: relative;
     display: flex;
     flex-direction: column;
@@ -287,12 +299,36 @@ body{
     word-wrap: break-word;
     background-color: #fff;
     background-clip: border-box;
-    border: 0 solid transparent;
+    border: 0 solid rgba(0,0,0,.125);
     border-radius: .25rem;
-    margin-bottom: 1.5rem;
-    box-shadow: 0 2px 6px 0 rgb(218 218 253 / 65%), 0 2px 6px 0 rgb(206 206 238 / 54%);
 }
-.me-2 {
-    margin-right: .5rem!important;
+
+.card-body {
+    flex: 1 1 auto;
+    min-height: 1px;
+    padding: 1rem;
+}
+
+.gutters-sm {
+    margin-right: -8px;
+    margin-left: -8px;
+}
+
+.gutters-sm>.col, .gutters-sm>[class*=col-] {
+    padding-right: 8px;
+    padding-left: 8px;
+}
+.mb-3, .my-3 {
+    margin-bottom: 1rem!important;
+}
+
+.bg-gray-300 {
+    background-color: #e2e8f0;
+}
+.h-100 {
+    height: 100%!important;
+}
+.shadow-none {
+    box-shadow: none!important;
 }
 </style>
