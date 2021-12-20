9ab204414f35112d12ddad0f0704a3b3c1675c9c
greydios vandita <greydios.vandita@gmail.com>
2021-12-20 08:42:12 +0800

    add change password button

--- pages/admin/profil.vue
+++ pages/admin/profil.vue
@@ -8,6 +8,7 @@
               <div class="d-flex flex-column align-items-center text-center">
                 <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
                 <div class="mt-3">
+                  <script />
                   <h3>John</h3>
                   <p class="text-secondary mb-1">
                     Full Stack Developer
@@ -15,12 +16,12 @@
                   <p class="text-muted font-size-sm">
                     Bay Area, San Francisco, CA
                   </p>
-                  <button class="btn btn-primary">
-                    Follow
+                  <button v-b-modal.modal-prevent-closing class="btn btn-primary">
+                    Change Password
                   </button>
-                  <button class="btn btn-outline-primary">
+                  <!-- <button class="btn btn-outline-primary">
                     Message
-                  </button>
+                  </button> -->
                   </a>
                 </div>
               </div>
